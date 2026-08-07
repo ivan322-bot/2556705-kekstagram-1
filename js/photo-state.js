4404276fa85b72b92ebd943a017bdfc281691298
Кириллов Иван Владимирович <vanyusha_kirillov_03@mail.ru>
2026-08-07 18:48:00 +0300

    сброс всего js кода

--- js/photo-state.js
+++
@@ -1,9 +0,0 @@
-let photos = [];
-
-const savePhotos = (newPhotos) => {
-  photos = newPhotos;
-};
-
-const getPhotoById = (id) => photos.find((photo) => photo.id === Number(id));
-
-export { savePhotos, getPhotoById };
