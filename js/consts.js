8ac5ad50227ee6a64eff4264676ce57bfc3d26f0
Кириллов Иван Владимирович <vanyusha_kirillov_03@mail.ru>
2026-08-09 20:25:24 +0300

    изменения

--- js/consts.js
+++ js/consts.js
@@ -1,20 +1,21 @@
-const MIN_PHOTO_ID = 1;
-const MAX_PHOTO_ID = 25;
-const MIN_PHOTO_URL = 1;
-const MAX_PHOTO_URL = 25;
-const DESCRIPTION_PHOTO = 'Всё отлично! В целом всё неплохо. Но не всё. Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально. Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше. Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше. Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'.split('.');
-const MIN_LENGTH_DESCRIPTION_PHOTO = 0;
-const MAX_LENGTH_DESCRIPTION_PHOTO = DESCRIPTION_PHOTO.length - 1;
-const MIN_LIKES_PHOTO = 15;
-const MAX_LIKES_PHOTO = 200;
-const MIN_COMMENTS = 0;
-const MAX_COMMENTS = 30;
-const MIN_COMMETNS_ID = 1;
-const MAX_COMMENTS_ID = 1000;
-const MIN_AVATAR: 1,
-const MAX_AVATAR: 6,
-const MESSAGE_IN_COMMENTS: 'Всё отлично! В целом всё неплохо. Но не всё. Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально. Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше. Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше. Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'.split('.'),
-const MIN_LENGTH_MESSAGE_IN_COMMENTS: 0,
-const MAX_LENGTH_MESSAGE_IN_COMMENTS: MESSAGE_IN_COMMENTS.length - 1,
+export const MIN_PHOTO_ID = 1;
+export const MAX_PHOTO_ID = 25;
+export const MIN_PHOTO_URL = 1;
+export const MAX_PHOTO_URL = 25;
+export const DESCRIPTION_PHOTO = 'Всё отлично! В целом всё неплохо. Но не всё. Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально. Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше. Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше. Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'.split('.');
+export const MIN_LENGTH_DESCRIPTION_PHOTO = 0;
+export const MAX_LENGTH_DESCRIPTION_PHOTO = DESCRIPTION_PHOTO.length - 1;
+export const MIN_LIKES_PHOTO = 15;
+export const MAX_LIKES_PHOTO = 200;
+export const MIN_COMMENTS = 0;
+export const MAX_COMMENTS = 30;
+export const MIN_COMMETNS_ID = 1;
+export const MAX_COMMENTS_ID = 1000;
+export const MIN_AVATA = 1;
+export const MAX_AVATAR = 6;
+export const MESSAGE_IN_COMMENTS = 'Всё отлично! В целом всё неплохо. Но не всё. Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально. Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше. Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше. Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'.split('.');
+export const MIN_LENGTH_MESSAGE_IN_COMMENTS = 0;
+export const MAX_LENGTH_MESSAGE_IN_COMMENTS = MESSAGE_IN_COMMENTS.length - 1;
+export const NAME = "Артем, Андрей, Сергей, Саша, Настя, Катя, Вера, Даша, Маша, Лера, Вика, Ева".split(',');
 
-export * as default
+
--- js/util.js
+++ js/util.js
@@ -1,3 +1,6 @@
+import './consts.js';
+import { DESCRIPTION_PHOTO, MAX_LIKES_PHOTO, MAX_PHOTO_ID, MAX_PHOTO_URL, MIN_LIKES_PHOTO, MIN_PHOTO_ID, MIN_PHOTO_URL } from './consts.js';
+
 function getRandomInteger (min, max) {
   const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
   const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
@@ -9,11 +12,30 @@ function getRandomValueArray (array) {
   return array[getRandomInteger (0, array[length - 1])];
 }
 
+function getUniqueInteger (min, max) {
+  const previosValues = [];
+  return function () {
+    let currentValue = getRandomInteger (min, max);
+    if (previosValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previosValues.includes(currentValue)) {
+      currentValue = getRandomInteger (min, max);
+    }
+    previosValues.push(currentValue);
+    return currentValue;
+  };
+}
+
 function getDataPhoto () {
   let objectWithDataPhoto = {};
   return function () {
     objectWithDataPhoto = {
-
+      id: getUniqueInteger(MIN_PHOTO_ID, MAX_PHOTO_ID),
+      url: `photos/${getUniqueInteger(MIN_PHOTO_URL, MAX_PHOTO_URL)}.jpg`,
+      description: getRandomValueArray(DESCRIPTION_PHOTO),
+      likes: getRandomInteger(MIN_LIKES_PHOTO, MAX_LIKES_PHOTO),
+      commentsgetRandomValueArray
     };
   };
 }
