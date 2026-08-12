f91c2e84675de5bac052b25fb7cf362469466b9a
Кириллов Иван Владимирович <vanyusha_kirillov_03@mail.ru>
2026-08-12 14:07:19 +0300

    создание массива объектов с данными для фотографии

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-import './util.js';
+// import './util.js';
+function examFoo () {
+  return function () {
+    return {
+      name: 'Mama',
+      surname: 'Kirillova',
+    };
+  };
+}
+
+const examArray = Array.from({length:3}, examFoo());
+
+console.log(examArray);
